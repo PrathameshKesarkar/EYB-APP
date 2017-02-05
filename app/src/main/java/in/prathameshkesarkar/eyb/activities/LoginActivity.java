8a348ff72bfd217dfadd7ca747a890b2561cb1c4
prathameshkesarkar <pratham.kearkar@gmail.com>
2017-02-05 18:15:29 +0530

    Add:Animation for Login and SignUp FIx: BlankActivity Loading at the start of the app

--- app/src/main/java/in/prathameshkesarkar/eyb/activities/LoginActivity.java
+++ app/src/main/java/in/prathameshkesarkar/eyb/activities/LoginActivity.java
@@ -1,9 +1,17 @@
 package in.prathameshkesarkar.eyb.activities;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import in.prathameshkesarkar.eyb.R;
 
 /**
@@ -11,9 +19,63 @@ import in.prathameshkesarkar.eyb.R;
  */
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_card)
+    View loginCardLayout;
+
+    @BindView(R.id.signup_card)
+    View signUpCardLayout;
+
+    private float scale;
+    private int distance = 9000;
+    Unbinder unbinder;
+
+    private AnimatorSet outAnimator, inAnimator;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        scale = getResources().getDisplayMetrics().density;
+        unbinder = ButterKnife.bind(this);
+
+        setCameraDistance();
+        initalizeAnimation();
+
+
+    }
+
+    @OnClick(R.id.signUpButton)
+    public void showSignUp(Button signUpButton) {
+        inAnimator.setTarget(signUpCardLayout);
+        outAnimator.setTarget(loginCardLayout);
+        outAnimator.start();
+        inAnimator.start();
+
+    }
+
+    @OnClick(R.id.signUpLogin)
+    public void showLogin(Button loginButton) {
+        inAnimator.setTarget(loginCardLayout);
+        outAnimator.setTarget(signUpCardLayout);
+        outAnimator.start();
+        inAnimator.start();
+    }
+
+    public void initalizeAnimation() {
+        outAnimator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate_out);
+        inAnimator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate_in);
+    }
+
+    public void setCameraDistance() {
+        loginCardLayout.setCameraDistance(distance * scale);
+        signUpCardLayout.setCameraDistance(distance * scale);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
